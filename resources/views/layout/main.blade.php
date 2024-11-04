1e2d0f84a3b052f14246439bcbb2344f4beac4a2
Jimmy <toluadejimi@gmail.com>
2024-11-04 23:44:09 +0100

    pushhhhhh

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -213,7 +213,7 @@
 
         </div>
         <ul class="messages">
-            <li style="color: white" class="other"><a style="color: white"  href="https://t.me/faddedsmsteam" target="_blank"><i class="bi bi-telegram"> </i> Get Support </a></li>
+            <li style="color: white" class="other"><a style="color: white"  href="https://t.me/faddedsms" target="_blank"><i class="bi bi-telegram"> </i> Get Support </a></li>
             <li style="color: white" class="other"><a style="color: white"  href="https://t.me/faddedsms" target="_blank"><i class="bi bi-telegram"> </i> Join our channel</a></li>
         </ul>
     </div>
