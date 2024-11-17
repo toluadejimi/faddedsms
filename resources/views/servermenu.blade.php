9a6f0646e3e6eb7d80ed99f06afe5b52e0867e81
Jimmy <toluadejimi@gmail.com>
2024-11-17 07:31:48 +0100

    pushhhhhh

--- resources/views/servermenu.blade.php
+++ resources/views/servermenu.blade.php
@@ -9,24 +9,24 @@
 
 
 
-<a class="btn btn-dark border-0 mb-2" href="/server3"
-   style="background: #3861ff"
->
-    SERVER 2  <span style="background: #f1060d" class="badge badge-success"> USA only</span>
-</a>
+{{--<a class="btn btn-dark border-0 mb-2" href="/server3"--}}
+{{--   style="background: #3861ff"--}}
+{{-->--}}
+{{--    SERVER 2  <span style="background: #f1060d" class="badge badge-success"> USA only</span>--}}
+{{--</a>--}}
 
 <a class="btn btn-dark border-0 " href="/world"
    style="background: #064175"
 >
-    SERVER 3  <span style="background: #f1060d" class="badge badge-success"> All countries</span>
+    SERVER 2  <span style="background: #f1060d" class="badge badge-success"> All countries</span>
 </a>
 
 
-<a class="btn btn-dark border-0" href="/cworld"
-   style="background: #fc6507"
->
-    SERVER 4 <span style="background: #f1060d" class="badge badge-success"> All Countries</span>
-</a>
+{{--<a class="btn btn-dark border-0" href="/cworld"--}}
+{{--   style="background: #fc6507"--}}
+{{-->--}}
+{{--    SERVER 4 <span style="background: #f1060d" class="badge badge-success"> All Countries</span>--}}
+{{--</a>--}}
 
 
 
